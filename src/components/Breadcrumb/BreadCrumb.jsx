341331871f3496483d23f1f6ccde0b27897e21fa
CadraAdmin <nikinprasadmscds18@gmail.com>
2024-02-20 18:18:52 +0530

    updates

--- src/components/Breadcrumb/BreadCrumb.jsx
+++ src/components/Breadcrumb/BreadCrumb.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import headerBackground from "./assets/img/headerBackground.jpg";
 
 function BreadCrumb({ page }) {
   return (
     <div className="page-title-area item-bg-2">
-      <div className="d-table">
+      <div
+        className="d-table"
+        style={{ background: `url(${headerBackground})` }}
+      >
         <div className="d-table-cell">
           <div className="container">
             <div className="page-title-content">
